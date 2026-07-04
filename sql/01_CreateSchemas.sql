011ffa160733dd2a7529c2fde59afbbab9203802
opanneh <57756200+opanneh@users.noreply.github.com>
2026-07-04 23:49:53 +0200

    Create Warehouse Schemas in SQL
    This script creates three schemas: Dimensions, Facts, and Reporting if they do not already exist. It also includes a success message upon completion.

---
+++ sql/01_CreateSchemas.sql
@@ -0,0 +1,43 @@
+-- ==========================================================
+-- Script    : 01_CreateSchemas.sql
+-- Purpose   : Create Warehouse Schemas
+-- Project   : Fabric AI Customer Intelligence
+-- ==========================================================
+
+-- Create Dimensions Schema
+
+IF NOT EXISTS (
+    SELECT *
+    FROM sys.schemas
+    WHERE name = 'Dimensions'
+)
+BEGIN
+    EXEC ('CREATE SCHEMA Dimensions');
+END;
+GO
+
+-- Create Facts Schema
+
+IF NOT EXISTS (
+    SELECT *
+    FROM sys.schemas
+    WHERE name = 'Facts'
+)
+BEGIN
+    EXEC ('CREATE SCHEMA Facts');
+END;
+GO
+
+-- Create Reporting Schema
+
+IF NOT EXISTS (
+    SELECT *
+    FROM sys.schemas
+    WHERE name = 'Reporting'
+)
+BEGIN
+    EXEC ('CREATE SCHEMA Reporting');
+END;
+GO
+
+PRINT 'Schemas created successfully.';
