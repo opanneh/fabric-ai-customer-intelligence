9cd72130f5492ef38fe209b2ea25f77841b3a2d3
opanneh <57756200+opanneh@users.noreply.github.com>
2026-07-04 23:48:34 +0200

    Drop all Gold Warehouse tables
    This script drops all gold warehouse tables including fact and dimension tables.

--- sql/00_Drop_Gold_Objects.sql
+++ sql/00_Drop_Gold_Objects.sql
@@ -1,1 +1,36 @@
+-- ==========================================================
+-- Script    : 00_Drop_Gold_Objects.sql
+-- Purpose   : Drop all Gold Warehouse Tables
+-- Project   : Fabric AI Customer Intelligence
+-- ==========================================================
 
+/*===========================================================
+    FACT TABLES
+===========================================================*/
+
+DROP TABLE IF EXISTS Facts.FactCustomerSentiment;
+GO
+
+DROP TABLE IF EXISTS Facts.FactReviews;
+GO
+
+DROP TABLE IF EXISTS Facts.FactSales;
+GO
+
+/*===========================================================
+    DIMENSION TABLES
+===========================================================*/
+
+DROP TABLE IF EXISTS Dimensions.DimPaymentMethod;
+GO
+
+DROP TABLE IF EXISTS Dimensions.DimDate;
+GO
+
+DROP TABLE IF EXISTS Dimensions.DimProduct;
+GO
+
+DROP TABLE IF EXISTS Dimensions.DimCustomer;
+GO
+
+PRINT 'All Gold tables dropped successfully.';
